4ae57078440d6e9aa045b923af15f41feb009204
Davis Okioma <davis.okioma@savannahinformatics.com>
2022-09-03 17:13:31 +0300

    refactor project

--- index.js
+++ index.js
@@ -1,89 +1,49 @@
-const button = document.querySelector("#btn")
-let inputEl = document.querySelector("#input-el")
-let userDiv = document.querySelector("#user-info")
-let userAvatar = document.querySelector("#avatar")
-let userName = document.querySelector("#name")
-let userBio = document.querySelector("#bio")
-let userBlog = document.querySelector("#blog")
-let url = document.querySelector("#url")
-let users = []
+const container = document.querySelector("#container");
+const button = document.querySelector("#btn");
+let inputEl = document.querySelector("#input-el");
+let userDiv = document.querySelector(".user-container");
+const API_URL = "https://api.github.com/users/";
 
-
-/*button.addEventListener("click", function(){
-    console.log("button clicked")
-    fetch(`https://api.github.com/users/${inputEl.value}`)
-    .then(result=>result.json())
-    .then((data)=>{
-        console.log(data)
-        let userAvatar = data["avatar_url"]
-        let userName = data.name
-        let userBio = data.bio
-        let userBlog = data.blog
-        let userUrl = data["html_url"]
-
-        avatar.style.backgroundImage = "url('userAvatar')"
-        name.innerHTML = `Name: ${userName}`
-        bio.innerHTML = `Bio: ${userBio}`
-        blog.innerHTML = `Blog: ${userBlog}`
-        url.innerHTML = `Github Page: ${userUrl}`
-
-    })
-        
-})*/
-
-
-
-let userInfo = {
-    userData: function(username){
-        return fetch(`https://api.github.com/users/${username}`)
-        .then(response => response.json())
-        .then((data)=>{
-            console.log(data)
-            this.displayData(data)})
-    },
-    displayData: function(data){
-        let {id} = data
-        let {name} = data
-        let {bio} = data
-        let {blog} = data
-        console.log(name, bio, blog, id)
-        userAvatar.src = `https://avatars.githubusercontent.com/u/${id}?v=4`
-        userName.innerHTML = name
-        userBio.innerHTML = bio
-        userBlog.innerHTML = blog
-    }
+function getUserInfo() {
+	try {
+		const username = inputEl.value;
+		fetch(`${API_URL}${username}`)
+			.then((response) => response.json())
+			.then(renderUserInfo);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
+function renderUserInfo(data) {
+	const { name, bio, blog, avatar_url } = data;
+	const userDiv = document.createElement("div");
+	const imageElement = document.createElement("img");
+	const infoDiv = document.createElement("div");
+	const nameElement = document.createElement("h1");
+	const bioElement = document.createElement("p");
+	const blogElement = document.createElement("p");
 
-button.addEventListener("click", function(){
-    userInfo.userData(inputEl.value)
-    inputEl.value = ""
-    users.push(userInfo)
-    users.forEach(function(user){
-        userDiv+=user
-    })
-})
+	userDiv.classList.add("user-container");
+	imageElement.classList.add("avatar");
+	infoDiv.classList.add("user-info");
 
-console.log(users)
+	imageElement.src = avatar_url;
+	imageElement.alt = name;
+	nameElement.innerText = `Name: ${name}`;
+	bioElement.innerText = `Bio: ${bio}`;
+	blogElement.innerText = `Blog: ${blog}`;
 
+	infoDiv.appendChild(nameElement);
+	infoDiv.appendChild(bioElement);
+	infoDiv.appendChild(blogElement);
 
+	userDiv.appendChild(imageElement);
+	userDiv.appendChild(infoDiv);
 
+	container.appendChild(userDiv);
 
+	inputEl.value = "";
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+button.addEventListener("click", getUserInfo);
